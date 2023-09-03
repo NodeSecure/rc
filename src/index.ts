d444c6405096f6dbfc3f468cbeba71ba8490f723
Thomas.G <gentilhomme.thomas@gmail.com>
2023-09-03 16:37:29 +0200

    feat: implement scanner configuration (#124)

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from "./functions/write.js";
 export * from "./functions/memoize.js";
 export * as CONSTANTS from "./constants.js";
 
-export { RC, JSONSchema, homedir } from "./rc.js";
+export { RC, Author, JSONSchema, homedir } from "./rc.js";
